153d007b2c52c1a7a4f3decd48ab26187b943419
Pedro <pete@daffodil.uk.com>
2015-01-03 07:58:31 +0000

    am confused

--- make_godoc.sh
+++ make_godoc.sh
@@ -3,4 +3,4 @@
 go run docfile.go  \
     -templates $GOPATH/revel.github.io/docs/godoc \
     -out $GOPATH/github.com/revel/revel.github.io/docs \
-    github.com/revel/revel/*.go
+    $GOPATH/github.com/revel/revel/*.go
