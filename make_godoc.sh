a5873110d7358cf878270d03d1829092b30f39f9
Pedro <pete@daffodil.uk.com>
2015-01-03 08:21:16 +0000

    still confused

--- make_godoc.sh
+++ make_godoc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo "GOPATH="
+echo $GOPATH
+
 go run docfile.go  \
-    -templates $GOPATH/revel.github.io/docs/godoc \
-    -out $GOPATH/github.com/revel/revel.github.io/docs \
-    $GOPATH/github.com/revel/revel/*.go
+    -templates $GOPATH/src/github.com/revel.github.io/docs/godoc \
+    -out $GOPATH/src/github.com/revel/revel.github.io/docs \
+    $GOPATH/src/github.com/revel/revel/*.go
